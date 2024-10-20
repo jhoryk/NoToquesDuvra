b5255b659889b5df243daafe3da6d372599b6404
jhoryk <jhorykjhoryk@gmail.com>
2024-10-20 16:02:56 +0200

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,24 @@
+let usuario = document.getElementById("usuario");
+let clave = document.getElementById("clave");
+
+document.getElementById("myButton").addEventListener("click", function () {
+  if (usuario.value === "richard") {
+    console.log("muy gay");
+  }
+
+  //Fizzbass
+  if (clave.value % 3 === 0 && clave.value % 2 === 0) {
+    console.log("El que lo lea es gay y si no lees tambien");
+  } else if (clave.value % 2 === 0) {
+    console.log("el que lo lea es gay");
+  } else if (clave.value % 3 === 0) {
+    console.log("y si no lees tambien");
+  }
+
+  // Otra cosa
+  if (usuario.value === "reyes") {
+    console.log("naguebona te pasaste de gay");
+  } else if (usuario.value == "richard reyes") {
+    console.log("eres el mega rey gay");
+  }
+});
